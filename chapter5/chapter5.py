80b4985129b0501485c8089bdf229c111e0ce2d0
Ubuntu <ubuntu@ip-172-31-18-234.eu-north-1.compute.internal>
2025-12-11 22:35:11 +0000

    more work

--- chapter5/chapter5.py
+++ chapter5/chapter5.py
@@ -44,7 +44,40 @@ inputs = torch.tensor([[16833, 3626, 6100], [40, 1107, 588]])
 targets = torch.tensor([[3626, 6100, 345 ], [1107, 588, 11311]])
 # [" effort moves you", " really like chocolate"]
 
-with torch.no_grad():
-	logits = model(inputs)     #disables gradient tracking
-probas = torch.softmax(inputs, dim=-1)
-print(probas.shape) 
+with torch.no_grad():              #disables gradient tracking
+	logits = model(inputs)     #logits are floats essentially 
+probas = torch.softmax(inputs, dim=-1) #this gets cast to probabilities which sum to 1 
+print(probas.shape)
+
+token_ids = torch.argmax(probas, dim=-1, keepdim=True) #choose the highest values 
+print("Token IDs:\n", token_ids)
+
+print(f"Targets batch 1: {token_ids_to_text(targets[0], tokenizer)}")
+print(f"Outputs batch 1:"
+f" {token_ids_to_text(token_ids[0].flatten(), tokenizer)}")
+
+text_idx = 0
+target_probas_1 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+print("Text 1:", target_probas_1)
+text_idx = 1
+target_probas_2 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+print("Text 2:", target_probas_2)
+
+log_probas = torch.log(torch.cat((target_probas_1, target_probas_2)))
+print(log_probas)
+
+avg_log_probas = torch.mean(log_probas)
+print(avg_log_probas)
+
+neg_avg_log_probas = avg_log_probas * -1
+print(neg_avg_log_probas)
+
+print("Logits shape:", logits.shape)
+print("Targets shape:", targets.shape)
+
+logits_flat = logits.flatten(0, 1)
+targets_flat = targets.flatten()
+print("Flattened logits:", logits_flat.shape)
+print("Flattened targets:", targets_flat.shape)
+
+
